f88ecbe08f3f5fc4c771c5159efb50292e59ee9e
Chatthana Janethanakarn <chattana.j@gmail.com>
2018-05-17 17:46:01 +0700

    Failure test

---
+++ spec/sample.test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const should = chai.should();
+
+const mockValue = 10;
+
+describe('Test mocking data', () => {
+  it('mockValue should be 10', (done) => {
+    mockValue.should.equal(1);
+    done();
+  });
+});
